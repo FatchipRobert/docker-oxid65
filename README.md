190bc73cda6d9df8592dd8d96b4c699cbd3e014a
Robert Müller <robert.mueller@fatchip.de>
2023-10-23 10:16:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Docker setup for Oxid 6.5
+
+This Docker setup installs a Oxid 6.5 shop with demodata.  
+It is meant to be used just like a real linux webserver.  
+So you have to connect to it via ssh and sftp.  
+It has no Docker volumes to not slow it down with file management and symlink stuff when used in Windows.
+This also means that when the containers are deleted the files in it are gone so beware.  
+I use it with PHPStorm in a remote host with SFTP access configuration.
+
+## Important URLs
+Shop: https://localhost  
+Admin: https://localhost/admin (Login:  admin // admin)  
+phpmyadmin: http://localhost:8080/ (Login: docker // docker)  
+SSH/SFTP: root@localhost Port: 2222 (Login: docker)
+
+## Docker setup
+
+### create container
+docker-compose build
+### fire up container
+docker-compose up -d  
+  
+To create multiple setups, name your setup manually:  
+docker-compose -pMYPROJECT up -d
---
+++ apache/docker/files/init_script.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Restart ssh server"
+/etc/init.d/ssh restart
+
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ];
+then
+    touch /$CONTAINER_FIRST_STARTUP
+
+    sudo -u www-data composer create-project --no-dev oxid-esales/oxideshop-project . dev-b-6.5-ce
+
+    chmod -R 777 /var/www/html
+
+    while ! mysqladmin ping -h"mysql.$DOMAIN" --silent; do
+        sleep 1
+        echo "Waiting for MYSQL server"
+    done
+
+    echo "MYSQL SERVER IS UP!"
+
+    echo "Copy config.inc.php file"
+    cp -f /oxid/config.inc.php source/config.inc.php
+
+    echo "Install demodata"
+    mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --host=mysql.$DOMAIN $MYSQL_DATABASE < /var/www/html/source/Setup/Sql/database_schema.sql
+    mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --host=mysql.$DOMAIN $MYSQL_DATABASE < /var/www/html/source/Setup/Sql/initial_data.sql
+    php /var/www/html/vendor/bin/oe-eshop-db_views_generate
+    mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -f --host=mysql.$DOMAIN $MYSQL_DATABASE < /var/www/html/vendor/oxid-esales/oxideshop-demodata-ce/src/demodata.sql
+    cp -R /var/www/html/vendor/oxid-esales/oxideshop-demodata-ce/src/out/pictures/ /var/www/html/source/out/pictures/
+fi
+
+echo "#####################################"
+echo "###### Docker setup completed! ######"
+echo "#####################################"
+
+exec "$@"
